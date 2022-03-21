08c1fab50cf96871f1fbdcb0e52694135ad76e44
Swadique N <nannatsadik@gmail.com>
2022-03-21 20:00:48 +0530

    initial items

---
+++ lib/screenhome.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ScreenHome extends StatefulWidget {
+  const ScreenHome({Key? key}) : super(key: key);
+
+  @override
+  State<ScreenHome> createState() => _ScreenHomeState();
+}
+
+class _ScreenHomeState extends State<ScreenHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
